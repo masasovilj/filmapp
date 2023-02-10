f012344db304129e563457aea6c5d0cfe2bcf502
masasovilj <95534935+masasovilj@users.noreply.github.com>
2023-02-10 17:29:15 +0100

    Direktor model

--- film_app/app/Models/Director.php
+++ film_app/app/Models/Director.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Director extends Model
 {
-    use HasFactory;
+    use HasFactory; 
     protected $fillable=['id','name','biography'];
 
     public function films(){
