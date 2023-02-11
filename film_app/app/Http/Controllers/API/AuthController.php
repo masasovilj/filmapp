4dd6ba88e609e17d84cd431d21522160ebd2fbcb
masasovilj <95534935+masasovilj@users.noreply.github.com>
2023-02-11 10:09:18 +0100

    Dodat AuthController zbog restapi

--- film_app/app/Http/Controllers/API/AuthController.php
+++ film_app/app/Http/Controllers/API/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
         ->json(['data'=>$user,'access_token'=>$token,'token_type'=>"Bearer",]);
     }
    
-
+    
     public function login(Request $request)
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
